4dc2d3cd811167be9bd7fad50bd332128cbbbad9
Jacob Vespers <aevespers2@gmail.com>
2026-07-21 19:10:55 -0700

    feat: export journal reconciliation contract

--- iris_verifier_contract/__init__.py
+++ iris_verifier_contract/__init__.py
@@ -16,6 +16,12 @@ from .contract import (
     strict_json_loads,
     validate_profile,
 )
+from .journal import (
+    journal_record_sha256,
+    reconcile_journal,
+    validate_journal_fixture_case,
+    validate_journal_record,
+)
 
 __all__ = [
     "ContractError",
@@ -23,11 +29,15 @@ __all__ = [
     "atomic_state_sha256",
     "canonical_json_bytes",
     "derive_protected_identifier",
+    "journal_record_sha256",
     "project_atomic_consume_and_record",
+    "reconcile_journal",
     "screen_synthetic_attempt_context",
     "strict_json_loads",
     "validate_atomic_fixture_case",
     "validate_atomic_operation",
     "validate_atomic_state",
+    "validate_journal_fixture_case",
+    "validate_journal_record",
     "validate_profile",
 ]
